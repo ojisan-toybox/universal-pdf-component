583e607d1c1373edf0334fc338e5b201e7d56a0a
sadness_ojisan <sadness.ojisan@gmail.com>
2021-01-09 21:28:01 +0900

    use loader

--- webpack.config.js
+++ webpack.config.js
@@ -19,7 +19,11 @@ module.exports = {
             loader: "ts-loader",
           },
         ],
-        exclude: /node_modules/,
+        include: [
+          path.resolve(__dirname, "src"),
+          path.resolve(__dirname, "node_modules/react-pdf"),
+          path.resolve(__dirname, "node_modules/react-pdf-viewer"),
+        ],
       },
     ],
   },
