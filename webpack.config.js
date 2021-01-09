e1474682c48d7bf3c85c57341b8c90626ab84e26
sadness_ojisan <sadness.ojisan@gmail.com>
2021-01-09 20:36:36 +0900

    use ie

--- webpack.config.js
+++ webpack.config.js
@@ -8,6 +8,7 @@ module.exports = {
     path: path.resolve(__dirname, "./dist"),
     filename: "build.js",
   },
+  devtool: "inline-source-map",
   target: ["web", "es5"],
   module: {
     rules: [
