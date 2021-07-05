fca87f8ca9031839ed026f21bd841d3b314553b3
sadness_ojisan <sadness.ojisan@gmail.com>
2021-07-05 22:40:38 +0900

    Update size

--- src/index.tsx
+++ src/index.tsx
@@ -6,23 +6,18 @@ const Main = () => {
     <div>
       <p>link: {url}</p>
       <h1>iframe</h1>
-      <iframe src={url} scrolling="no">
+      <iframe src={url} scrolling="no" width="800px" height="400px">
         <p>It appears your web browser doesn't support iframes.</p>
       </iframe>
       <h1>object</h1>
-      <object data={url} type="application/pdf">
-        <p>
-          It appears you don't have Adobe Reader or PDF support in this web
-          browser. <a href={url}>Click here to download the PDF</a>. Or{" "}
-          <a href="http://get.adobe.com/reader/" target="_blank">
-            click here to install Adobe Reader
-          </a>
-          .
-        </p>
-        <embed src={url} type="application/pdf" />
-      </object>
+      <object
+        data={url}
+        type="application/pdf"
+        width="800px"
+        height="400px"
+      ></object>
       <h1>object and embed</h1>
-      <object data={url} type="application/pdf">
+      <object data={url} type="application/pdf" width="800px" height="400px">
         <p>
           It appears you don't have Adobe Reader or PDF support in this web
           browser. <a href={url}>Click here to download the PDF</a>. Or{" "}
@@ -31,15 +26,13 @@ const Main = () => {
           </a>
           .
         </p>
-        <embed src={url} type="application/pdf" />
+        <embed src={url} type="application/pdf" width="800px" height="400px" />
       </object>
       <h1>embed</h1>
-      <embed width="500" height="375" src={url} type="application/pdf"></embed>
+      <embed width="800" height="400" src={url} type="application/pdf"></embed>
       <h1>google drive</h1>
       <embed
         src={`https://drive.google.com/viewerng/viewer?embedded=true&url=${url}`}
-        width="500"
-        height="375"
       ></embed>
       <h1>PDFJS</h1>
       <embed
